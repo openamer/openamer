NousResearch/hermes-agent

fix(goals): auto-pause goal loop on consecutive transport failures
When a goal_judge model has a broken API key (401), DNS failures, or
timeouts, the judge falls through to 'continue' (fail-open) but the
consecutive transport failures were not counted — only parse failures
were tracked.  With 3 consecutive parse failures the loop auto-pauses,
but with transport failures it looped forever (the Xiaomi 401 bug).

Changes:
- Add DEFAULT_MAX_CONSECUTIVE_TRANSPORT_FAILURES = 5
- Add consecutive_transport_failures counter to GoalState
- judge_goal() now returns a 5-tuple (verdict, reason, parse_failed,
  wait_directive, transport_failed) instead of 4-tuple
- Transport errors (API 401/5xx, timeouts) set transport_failed=True
- evaluate_after_turn() auto-pauses when consecutive transport failures
  reach the threshold, with a clear message naming the failing model
- All 101 tests updated and passing

tests/hermes_cli/test_goals.py
@@ -152,13 +152,13 @@ class TestJudgeGoal:
     def test_empty_goal_skipped(self):
         from hermes_cli.goals import judge_goal
 
-        verdict, _, _, _wd = judge_goal("", "some response")
+        verdict, _, _, _wd, _tf = judge_goal("", "some response")
         assert verdict == "skipped"
 
     def test_empty_response_continues(self):
         from hermes_cli.goals import judge_goal
 
-        verdict, _, _, _wd = judge_goal("ship the thing", "")
+        verdict, _, _, _wd, _tf = judge_goal("ship the thing", "")
         assert verdict == "continue"
 
     def test_no_aux_client_continues(self):
@@ -169,7 +169,7 @@ class TestJudgeGoal:
             "agent.auxiliary_client.get_text_auxiliary_client",
             return_value=(None, None),
         ):
-            verdict, _, _, _wd = goals.judge_goal("my goal", "my response")
+            verdict, _, _, _wd, _tf = goals.judge_goal("my goal", "my response")
         assert verdict == "continue"
 
     def test_api_error_continues(self):
@@ -182,7 +182,7 @@ class TestJudgeGoal:
             "agent.auxiliary_client.get_text_auxiliary_client",
             return_value=(fake_client, "judge-model"),
         ):
-            verdict, reason, _, _wd = goals.judge_goal("goal", "response")
+            verdict, reason, _, _wd, _tf = goals.judge_goal("goal", "response")
         assert verdict == "continue"
         assert "judge error" in reason.lower()
 
@@ -201,7 +201,7 @@ class TestJudgeGoal:
             "agent.auxiliary_client.get_text_auxiliary_client",
             return_value=(fake_client, "judge-model"),
         ):
-            verdict, reason, _, _wd = goals.judge_goal("goal", "agent response")
+            verdict, reason, _, _wd, _tf = goals.judge_goal("goal", "agent response")
         assert verdict == "done"
         assert reason == "achieved"
 
@@ -220,7 +220,7 @@ class TestJudgeGoal:
             "agent.auxiliary_client.get_text_auxiliary_client",
             return_value=(fake_client, "judge-model"),
         ):
-            verdict, reason, _, _wd = goals.judge_goal("goal", "agent response")
+            verdict, reason, _, _wd, _tf = goals.judge_goal("goal", "agent response")
         assert verdict == "continue"
         assert reason == "not yet"
 
@@ -309,7 +309,7 @@ class TestGoalManager:
         mgr = GoalManager(session_id="eval-sid-1")
         mgr.set("ship it")
 
-        with patch.object(goals, "judge_goal", return_value=("done", "shipped", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("done", "shipped", False, None, False)):
             decision = mgr.evaluate_after_turn("I shipped the feature.")
 
         assert decision["verdict"] == "done"
@@ -325,7 +325,7 @@ class TestGoalManager:
         mgr = GoalManager(session_id="eval-sid-2", default_max_turns=5)
         mgr.set("a long goal")
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "more work", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "more work", False, None, False)):
             decision = mgr.evaluate_after_turn("made some progress")
 
         assert decision["verdict"] == "continue"
@@ -343,7 +343,7 @@ class TestGoalManager:
         mgr = GoalManager(session_id="eval-sid-3", default_max_turns=2)
         mgr.set("hard goal")
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "not yet", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "not yet", False, None, False)):
             d1 = mgr.evaluate_after_turn("step 1")
             assert d1["should_continue"] is True
             assert mgr.state.turns_used == 1
@@ -454,7 +454,7 @@ class TestJudgeParseFailureAutoPause:
             "agent.auxiliary_client.get_text_auxiliary_client",
             return_value=(fake_client, "judge-model"),
         ):
-            verdict, _, parse_failed, _wd = goals.judge_goal("goal", "response")
+            verdict, _, parse_failed, _wd, _tf = goals.judge_goal("goal", "response")
         assert verdict == "continue"
         assert parse_failed is False
 
@@ -470,7 +470,7 @@ class TestJudgeParseFailureAutoPause:
             "agent.auxiliary_client.get_text_auxiliary_client",
             return_value=(fake_client, "judge-model"),
         ):
-            verdict, _, parse_failed, _wd = goals.judge_goal("goal", "response")
+            verdict, _, parse_failed, _wd, _tf = goals.judge_goal("goal", "response")
         assert verdict == "continue"
         assert parse_failed is True
 
@@ -484,7 +484,7 @@ class TestJudgeParseFailureAutoPause:
         mgr.set("do a thing")
 
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "judge returned empty response", True, None)
+            goals, "judge_goal", return_value=("continue", "judge returned empty response", True, None, False)
         ):
             d1 = mgr.evaluate_after_turn("step 1")
             assert d1["should_continue"] is True
@@ -513,7 +513,7 @@ class TestJudgeParseFailureAutoPause:
 
         # Two parse failures…
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "not json", True, None)
+            goals, "judge_goal", return_value=("continue", "not json", True, None, False)
         ):
             mgr.evaluate_after_turn("step 1")
             mgr.evaluate_after_turn("step 2")
@@ -521,7 +521,7 @@ class TestJudgeParseFailureAutoPause:
 
         # …then one clean reply resets the counter.
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "making progress", False, None)
+            goals, "judge_goal", return_value=("continue", "making progress", False, None, False)
         ):
             d = mgr.evaluate_after_turn("step 3")
             assert d["should_continue"] is True
@@ -536,12 +536,13 @@ class TestJudgeParseFailureAutoPause:
         mgr.set("goal")
 
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "judge error: RuntimeError", False, None)
-        ):
+            goals, "judge_goal", return_value=("continue", "judge error: RuntimeError", False, None, False)
+        ): 
             for _ in range(5):
                 d = mgr.evaluate_after_turn("still going")
                 assert d["should_continue"] is True
             assert mgr.state.consecutive_parse_failures == 0
+            assert mgr.state.consecutive_transport_failures == 0
             assert mgr.state.status == "active"
 
     def test_consecutive_parse_failures_persists_across_goalmanager_reloads(
@@ -555,7 +556,7 @@ class TestJudgeParseFailureAutoPause:
         mgr.set("persistent goal")
 
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "empty", True, None)
+            goals, "judge_goal", return_value=("continue", "empty", True, None, False)
         ):
             mgr.evaluate_after_turn("r")
             mgr.evaluate_after_turn("r")
@@ -763,7 +764,7 @@ class TestJudgeGoalWithSubgoals:
                    return_value=(_FakeClient, "fake-model")), \
              patch("agent.auxiliary_client.get_auxiliary_extra_body",
                    return_value=None):
-            verdict, reason, parse_failed, _wd = goals.judge_goal(
+            verdict, reason, parse_failed, wait_directive, transport_failed = goals.judge_goal(
                 "ship the feature",
                 "ok shipped",
                 subgoals=["write tests", "update docs"],
@@ -875,7 +876,7 @@ class TestWaitBarrier:
             assert mgr.is_waiting() is True
 
             # The judge must NOT be called while parked, and no turn is burned.
-            judge = MagicMock(return_value=("continue", "x", False, None))
+            judge = MagicMock(return_value=("continue", "x", False, None, False))
             with patch.object(goals, "judge_goal", judge):
                 decision = mgr.evaluate_after_turn("still waiting on CI")
 
@@ -907,7 +908,7 @@ class TestWaitBarrier:
         assert mgr.is_waiting() is False  # lazy auto-clear
         assert mgr.state.waiting_on_pid is None
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "more", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "more", False, None, False)):
             decision = mgr.evaluate_after_turn("process finished, here are results")
 
         assert decision["verdict"] == "continue"
@@ -924,7 +925,7 @@ class TestWaitBarrier:
         # is_waiting clears the stale barrier immediately.
         assert mgr.is_waiting() is False
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "go", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "go", False, None, False)):
             decision = mgr.evaluate_after_turn("response")
         assert decision["should_continue"] is True
 
@@ -1024,7 +1025,7 @@ class TestJudgeDrivenWait:
             # Judge sees the running process and says wait-on-pid.
             with patch.object(
                 goals, "judge_goal",
-                return_value=("wait", "CI watcher still running", False, {"pid": proc.pid}),
+                return_value=("wait", "CI watcher still running", False, {"pid": proc.pid}, False),
             ):
                 decision = mgr.evaluate_after_turn(
                     "Pushed the PR, watching CI.",
@@ -1058,7 +1059,7 @@ class TestJudgeDrivenWait:
         mgr.set("retry after backoff")
         with patch.object(
             goals, "judge_goal",
-            return_value=("wait", "rate limited", False, {"seconds": 120}),
+            return_value=("wait", "rate limited", False, {"seconds": 120}, False),
         ):
             decision = mgr.evaluate_after_turn("Hit a 429, backing off.")
         assert decision["verdict"] == "wait"
@@ -1088,7 +1089,7 @@ class TestJudgeDrivenWait:
         mgr.set("do work")
         with patch.object(
             goals, "judge_goal",
-            return_value=("continue", "more to do", False, None),
+            return_value=("continue", "more to do", False, None, False),
         ):
             decision = mgr.evaluate_after_turn(
                 "made progress",
@@ -1156,7 +1157,7 @@ class TestSessionTriggerBarrier:
         mgr.set("wait for the build to succeed")
         with patch.object(
             goals, "judge_goal",
-            return_value=("wait", "blocked on build", False, {"session_id": "proc_t4"}),
+            return_value=("wait", "blocked on build", False, {"session_id": "proc_t4"}, False),
         ):
             decision = mgr.evaluate_after_turn(
                 "Started the build watcher.",
@@ -1184,7 +1185,7 @@ class TestSessionTriggerBarrier:
 
         # Loop resumes with a real judge verdict.
         with patch.object(goals, "judge_goal",
-                          return_value=("continue", "build done", False, None)):
+                          return_value=("continue", "build done", False, None, False)):
             d3 = mgr.evaluate_after_turn("build succeeded")
         assert d3["should_continue"] is True
 
@@ -1526,7 +1527,7 @@ class TestContractAndBackgroundCompose:
         with patch("agent.auxiliary_client.get_text_auxiliary_client",
                    return_value=(client, "fake-model")), \
              patch("agent.auxiliary_client.get_auxiliary_extra_body", return_value=None):
-            verdict, reason, parse_failed, wait_directive = goals.judge_goal(
+            verdict, reason, parse_failed, wait_directive, transport_failed = goals.judge_goal(
                 "ship the PR",
                 "I pushed and started the CI watcher; waiting on it now.",
                 contract=GoalContract(verification="PR CI goes green"),
@@ -1558,7 +1559,7 @@ class TestContractAndBackgroundCompose:
         with patch("agent.auxiliary_client.get_text_auxiliary_client",
                    return_value=(client, "fake-model")), \
              patch("agent.auxiliary_client.get_auxiliary_extra_body", return_value=None):
-            verdict, reason, parse_failed, wait_directive = goals.judge_goal(
+            verdict, reason, parse_failed, wait_directive, transport_failed = goals.judge_goal(
                 "ship the PR",
                 "CI finished: 30 passed, 0 failed. Done.",
                 contract=GoalContract(verification="PR CI goes green"),

openamer/openamer

tests/hermes_cli/test_goals.py
@@ -152,13 +152,13 @@ class TestJudgeGoal:
     def test_empty_goal_skipped(self):
         from hermes_cli.goals import judge_goal
 
-        verdict, _, _, _wd = judge_goal("", "some response")
+        verdict, _, _, _wd, _tf = judge_goal("", "some response")
         assert verdict == "skipped"
 
     def test_empty_response_continues(self):
         from hermes_cli.goals import judge_goal
 
-        verdict, _, _, _wd = judge_goal("ship the thing", "")
+        verdict, _, _, _wd, _tf = judge_goal("ship the thing", "")
         assert verdict == "continue"
 
     def test_no_aux_client_continues(self):
@@ -169,7 +169,7 @@ class TestJudgeGoal:
             "agent.auxiliary_client.call_llm",
             side_effect=RuntimeError("No LLM provider configured"),
         ):
-            verdict, _, _, _wd = goals.judge_goal("my goal", "my response")
+            verdict, _, _, _wd, _tf = goals.judge_goal("my goal", "my response")
         assert verdict == "continue"
 
     def test_api_error_continues(self):
@@ -180,7 +180,7 @@ class TestJudgeGoal:
             "agent.auxiliary_client.call_llm",
             side_effect=RuntimeError("boom"),
         ):
-            verdict, reason, _, _wd = goals.judge_goal("goal", "response")
+            verdict, reason, _, _wd, _tf = goals.judge_goal("goal", "response")
         assert verdict == "continue"
         assert "judge error" in reason.lower()
 
@@ -193,7 +193,7 @@ class TestJudgeGoal:
                 choices=[MagicMock(message=MagicMock(content='{"done": true, "reason": "achieved"}'))]
             ),
         ):
-            verdict, reason, _, _wd = goals.judge_goal("goal", "agent response")
+            verdict, reason, _, _wd, _tf = goals.judge_goal("goal", "agent response")
         assert verdict == "done"
         assert reason == "achieved"
 
@@ -206,7 +206,7 @@ class TestJudgeGoal:
                 choices=[MagicMock(message=MagicMock(content='{"done": false, "reason": "not yet"}'))]
             ),
         ):
-            verdict, reason, _, _wd = goals.judge_goal("goal", "agent response")
+            verdict, reason, _, _wd, _tf = goals.judge_goal("goal", "agent response")
         assert verdict == "continue"
         assert reason == "not yet"
 
@@ -295,7 +295,7 @@ class TestGoalManager:
         mgr = GoalManager(session_id="eval-sid-1")
         mgr.set("ship it")
 
-        with patch.object(goals, "judge_goal", return_value=("done", "shipped", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("done", "shipped", False, None, False)):
             decision = mgr.evaluate_after_turn("I shipped the feature.")
 
         assert decision["verdict"] == "done"
@@ -311,7 +311,7 @@ class TestGoalManager:
         mgr = GoalManager(session_id="eval-sid-2", default_max_turns=5)
         mgr.set("a long goal")
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "more work", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "more work", False, None, False)):
             decision = mgr.evaluate_after_turn("made some progress")
 
         assert decision["verdict"] == "continue"
@@ -329,7 +329,7 @@ class TestGoalManager:
         mgr = GoalManager(session_id="eval-sid-3", default_max_turns=2)
         mgr.set("hard goal")
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "not yet", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "not yet", False, None, False)):
             d1 = mgr.evaluate_after_turn("step 1")
             assert d1["should_continue"] is True
             assert mgr.state.turns_used == 1
@@ -438,7 +438,7 @@ class TestJudgeParseFailureAutoPause:
             "agent.auxiliary_client.call_llm",
             side_effect=RuntimeError("connection reset"),
         ):
-            verdict, _, parse_failed, _wd = goals.judge_goal("goal", "response")
+            verdict, _, parse_failed, _wd, _tf = goals.judge_goal("goal", "response")
         assert verdict == "continue"
         assert parse_failed is False
 
@@ -450,7 +450,7 @@ class TestJudgeParseFailureAutoPause:
             "agent.auxiliary_client.call_llm",
             return_value=MagicMock(choices=[MagicMock(message=MagicMock(content=""))]),
         ):
-            verdict, _, parse_failed, _wd = goals.judge_goal("goal", "response")
+            verdict, _, parse_failed, _wd, _tf = goals.judge_goal("goal", "response")
         assert verdict == "continue"
         assert parse_failed is True
 
@@ -464,7 +464,7 @@ class TestJudgeParseFailureAutoPause:
         mgr.set("do a thing")
 
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "judge returned empty response", True, None)
+            goals, "judge_goal", return_value=("continue", "judge returned empty response", True, None, False)
         ):
             d1 = mgr.evaluate_after_turn("step 1")
             assert d1["should_continue"] is True
@@ -493,7 +493,7 @@ class TestJudgeParseFailureAutoPause:
 
         # Two parse failures…
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "not json", True, None)
+            goals, "judge_goal", return_value=("continue", "not json", True, None, False)
         ):
             mgr.evaluate_after_turn("step 1")
             mgr.evaluate_after_turn("step 2")
@@ -501,7 +501,7 @@ class TestJudgeParseFailureAutoPause:
 
         # …then one clean reply resets the counter.
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "making progress", False, None)
+            goals, "judge_goal", return_value=("continue", "making progress", False, None, False)
         ):
             d = mgr.evaluate_after_turn("step 3")
             assert d["should_continue"] is True
@@ -516,12 +516,13 @@ class TestJudgeParseFailureAutoPause:
         mgr.set("goal")
 
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "judge error: RuntimeError", False, None)
-        ):
+            goals, "judge_goal", return_value=("continue", "judge error: RuntimeError", False, None, False)
+        ): 
             for _ in range(5):
                 d = mgr.evaluate_after_turn("still going")
                 assert d["should_continue"] is True
             assert mgr.state.consecutive_parse_failures == 0
+            assert mgr.state.consecutive_transport_failures == 0
             assert mgr.state.status == "active"
 
     def test_consecutive_parse_failures_persists_across_goalmanager_reloads(
@@ -535,7 +536,7 @@ class TestJudgeParseFailureAutoPause:
         mgr.set("persistent goal")
 
         with patch.object(
-            goals, "judge_goal", return_value=("continue", "empty", True, None)
+            goals, "judge_goal", return_value=("continue", "empty", True, None, False)
         ):
             mgr.evaluate_after_turn("r")
             mgr.evaluate_after_turn("r")
@@ -732,7 +733,7 @@ class TestJudgeGoalWithSubgoals:
             return _FakeResp()
 
         with patch("agent.auxiliary_client.call_llm", side_effect=_fake_call_llm):
-            verdict, reason, parse_failed, _wd = goals.judge_goal(
+            verdict, reason, parse_failed, _wd, _tf = goals.judge_goal(
                 "ship the feature",
                 "ok shipped",
                 subgoals=["write tests", "update docs"],
@@ -837,7 +838,7 @@ class TestWaitBarrier:
             assert mgr.is_waiting() is True
 
             # The judge must NOT be called while parked, and no turn is burned.
-            judge = MagicMock(return_value=("continue", "x", False, None))
+            judge = MagicMock(return_value=("continue", "x", False, None, False))
             with patch.object(goals, "judge_goal", judge):
                 decision = mgr.evaluate_after_turn("still waiting on CI")
 
@@ -869,7 +870,7 @@ class TestWaitBarrier:
         assert mgr.is_waiting() is False  # lazy auto-clear
         assert mgr.state.waiting_on_pid is None
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "more", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "more", False, None, False)):
             decision = mgr.evaluate_after_turn("process finished, here are results")
 
         assert decision["verdict"] == "continue"
@@ -886,7 +887,7 @@ class TestWaitBarrier:
         # is_waiting clears the stale barrier immediately.
         assert mgr.is_waiting() is False
 
-        with patch.object(goals, "judge_goal", return_value=("continue", "go", False, None)):
+        with patch.object(goals, "judge_goal", return_value=("continue", "go", False, None, False)):
             decision = mgr.evaluate_after_turn("response")
         assert decision["should_continue"] is True
 
@@ -986,7 +987,7 @@ class TestJudgeDrivenWait:
             # Judge sees the running process and says wait-on-pid.
             with patch.object(
                 goals, "judge_goal",
-                return_value=("wait", "CI watcher still running", False, {"pid": proc.pid}),
+                return_value=("wait", "CI watcher still running", False, {"pid": proc.pid}, False),
             ):
                 decision = mgr.evaluate_after_turn(
                     "Pushed the PR, watching CI.",
@@ -1020,7 +1021,7 @@ class TestJudgeDrivenWait:
         mgr.set("retry after backoff")
         with patch.object(
             goals, "judge_goal",
-            return_value=("wait", "rate limited", False, {"seconds": 120}),
+            return_value=("wait", "rate limited", False, {"seconds": 120}, False),
         ):
             decision = mgr.evaluate_after_turn("Hit a 429, backing off.")
         assert decision["verdict"] == "wait"
@@ -1050,7 +1051,7 @@ class TestJudgeDrivenWait:
         mgr.set("do work")
         with patch.object(
             goals, "judge_goal",
-            return_value=("continue", "more to do", False, None),
+            return_value=("continue", "more to do", False, None, False),
         ):
             decision = mgr.evaluate_after_turn(
                 "made progress",
@@ -1118,7 +1119,7 @@ class TestSessionTriggerBarrier:
         mgr.set("wait for the build to succeed")
         with patch.object(
             goals, "judge_goal",
-            return_value=("wait", "blocked on build", False, {"session_id": "proc_t4"}),
+            return_value=("wait", "blocked on build", False, {"session_id": "proc_t4"}, False),
         ):
             decision = mgr.evaluate_after_turn(
                 "Started the build watcher.",
@@ -1146,7 +1147,7 @@ class TestSessionTriggerBarrier:
 
         # Loop resumes with a real judge verdict.
         with patch.object(goals, "judge_goal",
-                          return_value=("continue", "build done", False, None)):
+                          return_value=("continue", "build done", False, None, False)):
             d3 = mgr.evaluate_after_turn("build succeeded")
         assert d3["should_continue"] is True
 
@@ -1462,7 +1463,7 @@ class TestContractAndBackgroundCompose:
         }]
         with patch("agent.auxiliary_client.call_llm",
                    side_effect=self._capture_call_llm(captured)):
-            verdict, reason, parse_failed, wait_directive = goals.judge_goal(
+            verdict, reason, parse_failed, wait_directive, _tf = goals.judge_goal(
                 "ship the PR",
                 "I pushed and started the CI watcher; waiting on it now.",
                 contract=GoalContract(verification="PR CI goes green"),
@@ -1492,7 +1493,7 @@ class TestContractAndBackgroundCompose:
                        captured,
                        content='{"verdict": "done", "reason": "CI is green, evidence shown"}',
                    )):
-            verdict, reason, parse_failed, wait_directive = goals.judge_goal(
+            verdict, reason, parse_failed, wait_directive, _tf = goals.judge_goal(
                 "ship the PR",
                 "CI finished: 30 passed, 0 failed. Done.",
                 contract=GoalContract(verification="PR CI goes green"),